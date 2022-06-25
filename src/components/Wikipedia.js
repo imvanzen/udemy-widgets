85c6f4c59b78b9c7a63bd8cbf55eaa6370df84d4
J.Reczko <reczko.jakub@gmail.com>
2022-06-25 19:21:14 +0200

    Create the Wikipedia component

---
+++ src/components/Wikipedia.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Wikipedia = () => {
+    return (
+        <div className="wikipedia">
+            Wikipedia
+        </div>
+    )
+}
+
+export default Wikipedia
