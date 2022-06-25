a6cdcb25ff1d94dd07366b8445855c3bb7b4a645
J.Reczko <reczko.jakub@gmail.com>
2022-06-25 15:09:28 +0200

    Set static default items but use the useEffect hook instead of creating static variable

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Accordion from './components/Accordion';
 
 const App = () => {
     const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        setItems([
+            {
+                title: 'What is React?',
+                description: 'React is a fron end javascript library'
+            },
+            {
+                title: 'Why use React?',
+                description: 'React is a favorite JS library among engineers'
+            },
+            {
+                title: 'How do you use React?',
+                description: 'You use React by creating components'
+            }
+        ])
+    }, [])
 
     return (
         <div className='app'>
