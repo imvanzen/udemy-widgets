132cdd2224e9012e90bfceda6c585d8366ce57c3
J.Reczko <reczko.jakub@gmail.com>
2022-06-25 15:41:51 +0200

    Wrap App component with semantic ui classes

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
     }, [])
 
     return (
-        <div className='app'>
+        <div className='app ui container'>
             <Accordion items={items} />
         </div>
     )
