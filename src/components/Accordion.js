02ba85f0aa909b87f921998373eb2c27d4e7f502
J.Reczko <reczko.jakub@gmail.com>
2022-06-25 15:01:20 +0200

    Destructure the items prop from arguments in the accordion component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Accordion = () => {
+const Accordion = ({ items }) => {
     return (
         <div>Accordion</div>
     )
--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ const App = () => {
 
     return (
         <div className='app'>
-            <Accordion />
+            <Accordion items={items} />
         </div>
     )
 }
