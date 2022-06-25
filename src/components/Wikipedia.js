5258acf8acd919592ce664918b923eb4577ee1fd
J.Reczko <reczko.jakub@gmail.com>
2022-06-25 21:21:23 +0200

    Create state variable and make an input a controlled input

--- src/components/Wikipedia.js
+++ src/components/Wikipedia.js
@@ -1,11 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Wikipedia = () => {
+    const [term, setTerm] = useState("");
+
     return (
         <div className="wikipedia ui form">
             <div className="field">
                 <label>Enter Search Term</label>
-                <input type="text" />
+                <input
+                    onChange={e => setTerm(e.target.value)}
+                    type="text"
+                    value={term} />
             </div>
         </div>
     )
