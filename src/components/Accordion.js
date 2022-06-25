770d671b4337a7aff2a61906735813d01988d951
J.Reczko <reczko.jakub@gmail.com>
2022-06-25 15:44:55 +0200

    Map accordion items to the JSX and wrap accordion container with nice semantic ui classes

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
 const Accordion = ({ items }) => {
+    const renderedItems = items.map(item => {
+        return <div key={item.title}>
+            <div className='title active'>
+                <i className='dropdown icon' />
+                {item.title}
+            </div>
+            <div className='content active'>
+                <p>{item.description}</p>
+            </div>
+        </div>
+    })
+
     return (
-        <div className='accordion'>Accordion</div>
+        <div className='accordion ui styled accordion'>
+            {renderedItems}
+        </div>
     )
 }
 
