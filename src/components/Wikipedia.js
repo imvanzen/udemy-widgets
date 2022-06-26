b09ac930310e173cf44d0c37486d6f0ed53e6918
J.Reczko <reczko.jakub@gmail.com>
2022-06-26 09:06:33 +0200

    Call the wiki api when typing in search bar

--- src/components/Wikipedia.js
+++ src/components/Wikipedia.js
@@ -1,18 +1,36 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
+import wikipedia from '../api/wikipedia'
 
 const Wikipedia = () => {
-    const [term, setTerm] = useState("");
+    const [term, setTerm] = useState('')
+    const [results, setResults] = useState([])
+
+    useEffect(() => {
+        if (!term) return
+        async function getResults() {
+            const response = await wikipedia.get('', {
+                params: { srsearch: term }
+            })
+            setResults(response.data.query.search)
+        }
+        getResults()
+    }, [term])
 
     return (
-        <div className="wikipedia ui form">
-            <div className="field">
-                <label>Enter Search Term</label>
-                <input
-                    onChange={e => setTerm(e.target.value)}
-                    type="text"
-                    value={term} />
+        <>
+            <div className="wikipedia ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input
+                        onChange={e => setTerm(e.target.value)}
+                        type="text"
+                        value={term} />
+                </div>
             </div>
-        </div>
+            <div className='wikipedia-results'>
+                {results.length}
+            </div>
+        </>
     )
 }
 
