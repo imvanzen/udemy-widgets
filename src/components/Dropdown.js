4ed282d4ff69a902c26b128b2f12e1ecd84bf712
J.Reczko <reczko.jakub@gmail.com>
2022-06-26 12:12:44 +0200

    Build basick ui for dropdown and map options to the jsx

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const Dropdown = () => {
+const Dropdown = ({ options }) => {
+    const renderedOptions = options.map(({ label, value }) => (
+        <div key={value} className='item'>
+            {label}
+        </div>
+    ))
+
     return (
-        <div className='dropdown'>
-            dropdown
+        <div className='dropdown ui form'>
+            <div className='field'>
+                <label className='label'>Select a Color</label>
+                <div className='ui selection dropdown visible active'>
+                    <i className='dropdown icon' />
+                    <div className='text'>Select Color</div>
+                    <div className='menu visible transition'>
+                        {renderedOptions}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
