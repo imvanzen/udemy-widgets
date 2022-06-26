7783b635f64d884dae70767a0321798a05c7ea8d
J.Reczko <reczko.jakub@gmail.com>
2022-06-26 09:54:15 +0200

    Add button which takes user to the search result on the wikipedia

--- src/components/Wikipedia.js
+++ src/components/Wikipedia.js
@@ -22,7 +22,13 @@ const Wikipedia = () => {
 
     const renderedResults = results.map(result => (
         <div className="item" key={result.pageid}>
-            <div className="content">
+            <div className='right floated content'>
+                <a
+                    className='ui button'
+                    href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    target='_blank'>Go</a>
+            </div>
+            <div className='content'>
                 <div className="header">{result.title}</div>
                 <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
             </div>
