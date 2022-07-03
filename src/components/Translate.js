dc0167d7a1e5b44482316115e4ff121bc29b3829
J.Reczko <reczko.jakub@gmail.com>
2022-07-03 14:36:13 +0200

    Create a translate component

---
+++ src/components/Translate.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Translate = () => {
+
+    return (
+        <div className="translate">
+            Translate
+        </div>
+    )
+}
+
+export default Translate
