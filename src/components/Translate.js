249ebe72d85aea0c233cb4a78a3cb52acdc60bb3
J.Reczko <reczko.jakub@gmail.com>
2022-07-03 20:00:48 +0200

    Display input in the translate component

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -17,16 +17,27 @@ const options = [
 ]
 
 const Translate = () => {
+    const [text, setText] = useState('')
     const [language, setLanguage] = useState(options[0])
 
     return (
         <div className='translate ui'>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Text</label>
+                    <input
+                        type='text'
+                        value={text}
+                        onChange={e => setText(e.target.value)} />
+                </div>
+            </div>
             <Dropdown
                 label='Select a Language'
                 options={options}
                 selected={language}
                 onSelectedChange={setLanguage} />
             <div>{language.label}</div>
+            <div>{text}</div>
         </div>
     )
 }
