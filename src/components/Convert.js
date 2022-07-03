578eada723329bbb6b0175be3fd709ad5fc11478
J.Reczko <reczko.jakub@gmail.com>
2022-07-03 20:20:38 +0200

    Create the Convert component

---
+++ src/components/Convert.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react"
+
+const Translate = ({
+    language,
+    text
+}) => {
+    return (
+        <div className='convert ui'>
+            Convert
+        </div>
+    )
+}
+
+export default Translate
