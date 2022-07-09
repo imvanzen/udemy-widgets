fe2d77988a55b163ecef15eed9076d5ea988c86d
J.Reczko <reczko.jakub@gmail.com>
2022-07-09 22:59:17 +0200

    Persist default user experience when clicking the link with alt or cmd keys pushed

--- src/Link.js
+++ src/Link.js
@@ -7,6 +7,7 @@ const Link = ({ href, className, children }) => {
             : className
     }
     const onClick = e => {
+        if (e.metaKey || e.ctrlKey) return
         e.preventDefault();
         window.history.pushState({}, '', href)
         const navEvent = new PopStateEvent('popstate')
