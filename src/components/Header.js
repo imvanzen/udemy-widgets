4f8bc5fabd1c41f7bae8bb114a9dd72d8cdcb276
J.Reczko <reczko.jakub@gmail.com>
2022-07-09 21:57:10 +0200

    Create header component containing menu

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Header = () => {
+    const isActive = pathname => {
+        return window.location.pathname === pathname
+            ? 'item active'
+            : 'item'
+    }
+
+    return (
+        <div className="ui pointing menu">
+            <a href='/' className={isActive('/')}>
+                Accordion
+            </a>
+            <a href='/wikipedia' className={isActive('/wikipedia')}>
+                Wikipedia
+            </a>
+            <a href='/dropdown' className={isActive('/dropdown')}>
+                Dropdown
+            </a>
+            <a href='/translate' className={isActive('/translate')}>
+                Translate
+            </a>
+        </div>
+    )
+}
+
+export default Header;
